e3bf9a55a0bdd6412e7e557fae9c0255094f98e2
gorohovIlya <ilya.gorohov.404@gmail.com>
2025-05-24 08:01:22 +0300

    correction

--- figures_and_colors/main.py
+++ figures_and_colors/main.py
@@ -3,23 +3,35 @@ import matplotlib.pyplot as plt
 from skimage.morphology import label
 from collections import defaultdict
 from skimage.measure import regionprops
+
+
+def truncate(values, decimals):
+    factor = 10 ** decimals
+    return np.floor(values * factor) / factor
+
+
 image = plt.imread("balls_and_rects.png")
-colors = np.unique(image.reshape(-1, image.shape[-1]), axis=0)
+
+rounded_image = truncate(image, 1)
+
+colors = np.unique(rounded_image.reshape(-1, rounded_image.shape[-1]), axis=0)
+
 c = defaultdict(lambda: 0)
 r = defaultdict(lambda: 0)
-t_image = image.copy()
+
 for i in colors[1:]:
     key = ", ".join([str(j) for j in i])
-    mask = np.all(t_image == i,axis=-1)
+    mask = np.all(rounded_image == i, axis=-1)
     labeled = label(mask)
     regions = regionprops(labeled)
+
     for reg in regions:
         if reg.eccentricity == 0:
             c[key] += 1
         else:
             r[key] += 1
-print("Circles:", "\n", c)
-c_sum = sum(c.values())
-print("Rectangles:", "\n", r)
-r_sum = sum(r.values())
-print(f"Total circles: {c_sum}, total rectangles: {r_sum}, total: {c_sum+r_sum}")
+
+print("Circles:", "\n", dict(c))
+print("Rectangles:", "\n", dict(r))
+print(f"Total circles: {sum(c.values())}, total rectangles: {sum(r.values())}, total: {sum(c.values()) + sum(r.values())}")
+print(f"Total colors: {len(colors)}")
